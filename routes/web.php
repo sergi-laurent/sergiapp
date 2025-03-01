26ee49052049eeb8aa55efa5126e3dfc434bfa6d
sergi-laurent <sergilaurents@gmail.com>
2025-03-01 09:48:11 +0100

    REFACTOR 🔨 : add a controller for weclome page

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+Route::get('/', [\App\Http\Controllers\WelcomeController::class, 'index'])->name('welcome');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
