fd15e46d98b60db8f845cc22b2d0cba83a268a46
sergi-laurent <sergilaurents@gmail.com>
2025-03-01 14:12:39 +0100

    REFACTOR 🔨 : Clean DatabaseSeeder + Generate users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+//we import our models from the App\Models directory
 use App\Models\Article;
+
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,12 +16,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+       
        Article::factory(count:10)->create();
+       User::factory(count:10)->create();
     }
 }
