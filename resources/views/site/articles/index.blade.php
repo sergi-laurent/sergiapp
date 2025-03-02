4f86fb7ad6d77e41266f482f06fbe10424222d0b
sergi-laurent <sergilaurents@gmail.com>
2025-03-02 17:41:51 +0100

    DESIGN 🎨 : Make our articles page as a Gallery

--- resources/views/site/articles/index.blade.php
+++ resources/views/site/articles/index.blade.php
@@ -1,10 +1,15 @@
 <x-site-layout>
 
-	<h1 class="font-bold text-2xl">Hello from the view</h1>
+	<h1 class="font-bold text-4xl">Hello from the view</h1>
 	
-	<ul class="list-disc pl-4">
+	<ul class="grid grid-cols-3 gap-12 mt-12">
 	    @foreach($articles as $article)
-	        <li class="underline"><a href="/articles/{{$article->id}}"> {{$article->title}}</a></li>
+	        <li class="p-2 border-t border-t-black hover:bg-purple-50">
+				<a href="/articles/{{$article->id}}">
+					<h3 class="font-bold text-2xl">{{$article->title}}</h3>
+					<p class="line-clamp-2">{{$article->content}}</p>
+				</a>
+			</li>
 	    @endforeach
 	</ul>
     
