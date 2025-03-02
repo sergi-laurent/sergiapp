48267598f26ef5642d9b8ad579aac704353e9613
sergi-laurent <sergilaurents@gmail.com>
2025-03-02 16:53:53 +0100

    DESIGN 🎨 : Add author name below article title

--- resources/views/site/articles/show.blade.php
+++ resources/views/site/articles/show.blade.php
@@ -1,5 +1,6 @@
 <x-site-layout>
 	<h1 class="font-bold mb-4 text-4xl">{{$article->title}}</h1>
+	<div class="mb-8">Article by <span class="font-semibold">{{$article->author->name}}</span></div>
 
 	<p>
         {{$article->content}}
