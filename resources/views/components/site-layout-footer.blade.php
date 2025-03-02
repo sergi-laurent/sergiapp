3945787c5172289133a295ec1cb147257c4f7d14
sergi-laurent <sergilaurents@gmail.com>
2025-03-02 16:52:11 +0100

    FEATURE ✨ : Move header and footer to separate components

---
+++ resources/views/components/site-layout-footer.blade.php
@@ -0,0 +1,5 @@
+<div class="bg-purple-700 text-purple-50 mt-16">
+        <div class="max-w-7xl m-auto">
+            <div>Footer</div>
+        </div>
+</div>
--- resources/views/components/site-layout.blade.php
+++ resources/views/components/site-layout.blade.php
@@ -8,28 +8,12 @@
   </head>
 
   <body>
-    <div class="bg-purple-700 text-purple-50 mb-6">
-        <div class="max-w-7xl m-auto flex justify-between items-center h-10">
-            <div class="font-bold text-lg">TucTuc</div>
-            <ul class="flex gap-x-4">
-                <li><a href="/articles">Articles</a></li>
-                <li><a href="">Authors</a></li>
-                <li><a href="">Keywords</a></li>
-                <li><a href="">Contact Us</a></li>
-            </ul>
-            <div>Login Comes Here</div>
-        </div>
-    </div>
+    <x-site-layout-header/>
 
     <div class="max-w-7xl m-auto">
         {{$slot}}
     </div>
 
-    <div class="bg-purple-700 text-purple-50 mt-16">
-        <div class="max-w-7xl m-auto">
-            <div>Footer</div>
-        </div>
-    </div>
-
+    <x-site-layout-footer/>
   </body>
 </html>
