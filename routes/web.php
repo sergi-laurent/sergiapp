618816d16bdfcdb6f9ed8a8410750eccd2ddc7e9
sergi-laurent <sergilaurents@gmail.com>
2025-03-05 08:59:52 +0100

    REFACTOR 🔨 : Switch to resourceful routes for articles

--- routes/web.php
+++ routes/web.php
@@ -5,20 +5,7 @@ use Livewire\Volt\Volt;
 
 Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-
-
-//Route for the Article index
-Route::get('/articles', [\App\Http\Controllers\ArticleController::class, 'index']);
-//Route to create and post a new Article
-Route::get('/articles/create', [\App\Http\Controllers\ArticleController::class, 'create']);
-Route::post('/articles', [\App\Http\Controllers\ArticleController::class, 'store']);
-//Route for the Article show
-Route::get('/articles/{article}', [\App\Http\Controllers\ArticleController::class, 'show']);
-//Route to edit an article
-Route::get('/articles/{article}/edit', [\App\Http\Controllers\ArticleController::class, 'edit']);
-Route::put('/articles/{article}', [\App\Http\Controllers\ArticleController::class, 'update']);
-//Route to delete an article
-Route::delete('/articles/{article}', [\App\Http\Controllers\ArticleController::class, 'destroy']);
+Route::resource('articles', \App\Http\Controllers\ArticleController::class); 
 
 
 
