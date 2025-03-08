f7d8bee7730b7e6451710ba162bb093e969b07a5
sergi-laurent <sergilaurents@gmail.com>
2025-03-08 18:11:21 +0100

    FEATURE ✨ : Articles list with Edit/Delete buttons in the User Admin Dashboard

--- resources/views/admin/articles/index.blade.php
+++ resources/views/admin/articles/index.blade.php
@@ -7,19 +7,31 @@
 	</div>
 
 	
-	<ul class="grid grid-cols-3 gap-12 mt-12">
+	<ul class="mt-12">
 	    @foreach($articles as $article)
 			
-	        <li class="p-2 border-t border-t-black hover:bg-purple-50">
-				<a href="{{route('admin.articles.show', $article)}}">
-					<h3 class="font-bold text-2xl">{{$article->title}}</h3>
-					<p class="line-clamp-2">{{$article->content}}</p>
-				</a>
+	        <li class="p-2 hover:bg-gray-50 flex justify-between">
+				
+					<a href="{{route('admin.articles.show', $article)}}">
+						<h3 class="font-bold text-2xl">{{$article->title}}</h3>
+						<p class="line-clamp-2">{{$article->content}}</p>
+					</a>
+                    
+					<div class="flex justify-center items-center gap-2">
+						<div>
+							<a href="{{route('admin.articles.edit', $article)}}" 
+							class="bg-purple-50 text-purple-500 p-1 px-2 uppercase hover:font-semibold rounded-sm">
+								Edit
+							</a>
+						</div>
+                    <form action="{{route('admin.articles.destroy', $article)}}" method="post">
+                        @method('DELETE')
+                        @csrf
+                        <button type="submit" class="bg-purple-50 text-purple-500 p-1 px-2 uppercase hover:font-semibold rounded-sm">Delete</button>
+                    </form>
+				</div>            
 			</li>
 	    @endforeach
 	</ul>
 
-
-	
-    
 </x-site-layout>
