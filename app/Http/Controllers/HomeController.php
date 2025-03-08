eb2dcc07c901d9404e9acb3cf561c646916241c7
sergi-laurent <sergilaurents@gmail.com>
2025-03-08 11:23:41 +0100

    FEATURE ✨ : Implement Welcome Screen with Blog style

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+namespace App\Http\Controllers;
+use App\Models\Article;
+use App\Models\User;
+
+
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view(view: 'welcome');
+        $articles=Article::take(4)->latest()->get();
+        $authors = User::whereHas('articles')->orderBy('name')->get();
+
+        return view('welcome', ['articles'=>$articles, 'authors'=>$authors]);
     }
 }
