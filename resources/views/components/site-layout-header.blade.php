6e34a0d35252851ff678a96fcef5201fce800535
sergi-laurent <sergilaurents@gmail.com>
2025-03-08 11:39:42 +0100

    FEATURE ✨ : Add button to Log in / Admin Dashboard (if already logged in)

--- resources/views/components/site-layout-header.blade.php
+++ resources/views/components/site-layout-header.blade.php
@@ -6,6 +6,12 @@
                     <li class="hover:font-semibold hover:text-white"><a href="{{$url}}">{{$label}}</a></li>
                 @endforeach
             </ul>
-            <div>Login Comes Here</div>
+            <div class="bg-purple-50 text-purple-500 p-1 rounded-sm px-2">
+                @if(auth()->user() != null)
+                    <a href="{{ route('admin.dashboard') }}">{{ auth()->user()->name }} </a>
+                @else
+                    <a href="{{route('login')}}">Log in</a>
+                @endif
+            </div>
         </div>
 </div>
