60cfe25b29984a81ccd99857b795aecf5227bdec
sergi-laurent <sergilaurents@gmail.com>
2025-03-08 14:37:43 +0100

    DESIGN 🎨 :  Layout of the Admin Dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,17 +1,24 @@
 <x-site-layout>
 
-    Admin Dashboard
+    <div>
+        Admin Dashboard
+        Your name is {{ auth()->user()->name }}
+    </div>
 
-    Your name is {{ auth()->user()->name }}
+    
 
     <div>
-        <h2>Your articles</h2>
+        <div class="flex justify-between">
+            <h2 class="font-bold text-2xl">Your articles</h2>  
+
+            <a href="{{route('admin.articles.create')}}" class="bg-purple-500 text-purple-50 uppercase p-2 hover:font-semibold rounded-sm">Create Article</a>
+        </div>
+
         <ul class="list-disc pl-4">
             @foreach(auth()->user()->articles as $article)
                 <li>{{ $article->title }}</li>
             @endforeach
         </ul>
-        
     
     </div>
 
